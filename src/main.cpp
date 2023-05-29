dd75d1eace3e847eb654f8f56788440064e86f20
Duong Trieu <duongtrieuelec@gmail.com>
2023-05-29 16:01:58 +0700

    "Update 29/5/2023"

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <Ticker.h>
+#include <EEPROM.h>
 
 // WiFi settings
 const char* ssid = "Lan Huong 5G";
 const char* password = "cohuong92";
 
+// AP settings
+const char* ap_ssid = "ESP8266-E12-AP";
+const char* ap_password = "12345678";
+
 // MQTT settings
 const char* mqttServer = "broker.hivemq.com";
 const int mqttPort = 1883;
-const char* mqttTopic = "esp8266/pirsensor";
+const char* mqttTopic_PC2uC = "esp8266/PC2uC";
+const char* mqttTopic_uC2PC = "esp8266/uC2PC";
 
 // Pin definitions
 const int inputPin = 2;
@@ -22,6 +28,7 @@ volatile bool motionDetected = false;
 volatile bool buttonPressed = false;
 volatile bool output1State = true;
 volatile bool output2State = false;
+volatile bool timerExpired = false;
 
 typedef std::function<void()> TimerCallbackFunction;
 Ticker timer;
@@ -34,7 +41,7 @@ void motionDetectedISR();
 void timer1Callback();
 void startTimer(int, TimerCallbackFunction);
 void stopTimer();
-void mqttCallback(char* , byte* , unsigned int );
+void mqttCallback(char*, byte*, unsigned int);
 void connectToMqtt();
 void publishToMqtt(String);
 
@@ -53,11 +60,9 @@ void setup() {
   digitalWrite(outputPin2, output2State);
 
   // Initialize WiFi connection
-  WiFi.begin(ssid, password);
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-  }
-  Serial.println("WiFi connected");
+  WiFi.mode(WIFI_AP); // Set WiFi mode to AP
+  WiFi.softAP(ap_ssid, ap_password); // Set AP credentials
+  Serial.println("AP created with SSID: " + String(ap_ssid) + " and password: " + String(ap_password));
 
   // Initialize MQTT client
   mqttClient.setServer(mqttServer, mqttPort);
@@ -70,7 +75,14 @@ void setup() {
 void loop() {
   // Check for MQTT connection
   if (!mqttClient.connected()) {
-    connectToMqtt();
+    //connectTo continue from the previous answer, here is the rest of the modified code:
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("WiFi connected");
+    mqttClient.connect("ESP8266Client");
+  } else {
+    Serial.println("AP mode, IP address: " + WiFi.softAPIP().toString());
+    mqttClient.connect("ESP8266Client");
   }
   mqttClient.loop();
 
@@ -81,6 +93,26 @@ void loop() {
     startTimer(1500, timer1Callback);
     motionDetected = false;
     publishToMqtt("motion_detected");
+    mqttClient.publish(mqttTopic_uC2PC, "Motion Detected"); 
+    // send message to topic esp8266/uC2PC
+  }
+  // Handle timer expiry
+  if (timerExpired) {
+    timerExpired = false;
+    if (motionDetected) {
+      mqttClient.publish(mqttTopic_uC2PC, "Detect People"); 
+      // send message to topic esp8266/uC2PC
+    } else {
+      mqttClient.publish(mqttTopic_uC2PC, "Objects just pass by"); 
+      // send message to topic esp8266/uC2PC
+    }
+  }
+  unsigned long lastPublishTime = 0;
+  if (!motionDetected && !timerExpired) {
+    if (millis() - lastPublishTime > 2000) {
+      lastPublishTime = millis();
+      mqttClient.publish(mqttTopic_uC2PC, "Not detected"); // gửi thông điệp tới topic esp8266/uC2PC
+    }
   }
 
   // Handle button press
@@ -96,6 +128,7 @@ void loop() {
     publishToMqtt("button_pressed");
   }
 }
+}
 
 void motionDetectedISR() {
   motionDetected = true;
@@ -104,10 +137,11 @@ void motionDetectedISR() {
 void timer1Callback() {
   output1State = true;
   digitalWrite(outputPin1, output1State);
+  timerExpired = true;
 }
 
 void startTimer(int duration, TimerCallbackFunction callback) {
-  timer.attach_ms(duration, callback);
+ timer.attach_ms(duration, callback);
 }
 
 void stopTimer() {
@@ -129,22 +163,51 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
     detachInterrupt(digitalPinToInterrupt(inputPin));
     publishToMqtt("output2_turned_off");
   }
-}
+  if (message == "turn_on") {
+    output2State = true;
+    digitalWrite(outputPin2, output2State);
+    detachInterrupt(digitalPinToInterrupt(inputPin));
+    publishToMqtt("output2_turned_on");
+  }
+  // Check if message contains SSID and password
+  if (message.startsWith("SSID=") && message.indexOf("&PWD=") != -1) {
+    // Extract SSID and password from message
+    String ssid = message.substring(5, message.indexOf("&PWD="));
+    String password = message.substring(message.indexOf("&PWD=") + 5);
 
+    // Save SSID and password to EEPROM
+    EEPROM.begin(512);
+    for (unsigned int i = 0; i < ssid.length(); i++) {
+      EEPROM.write(i, ssid[i]);
+    }
+    for (unsigned int i = 0; i < password.length(); i++) {
+      EEPROM.write(i + ssid.length() + 1, password[i]);
+    }
+    EEPROM.commit();
+    EEPROM.end();
+
+    Serial.println("SSID and password saved to EEPROM");
+
+    // Restart ESP8266 toapply new WiFi credentials
+    ESP.restart();
+  }
+}
 
 void connectToMqtt() {
   while (!mqttClient.connected()) {
     Serial.println("Connecting to MQTT broker...");
-    if (mqttClient.connect("ESP8266Client")) {
-      Serial.println("MQTT client connected");
-      mqttClient.subscribe(mqttTopic);
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("WiFi connected");
+      mqttClient.connect("ESP8266Client");
     } else {
-      Serial.println("MQTT connection failed");
-      delay(5000);
+      Serial.println("AP mode, IP address: " + WiFi.softAPIP().toString());
+      mqttClient.connect("ESP8266Client");
     }
+    delay(5000);
   }
+  mqttClient.subscribe(mqttTopic_PC2uC);
 }
 
 void publishToMqtt(String message) {
-  mqttClient.publish(mqttTopic, message.c_str());
+  mqttClient.publish(mqttTopic_PC2uC, message.c_str());
 }
